52055602af3bacfcfb70abbd63a428be8d4fd26a
Maz <mazicky@gmail.com>
2016-05-22 22:42:48 +0900

    두 개 이상의 prime factor를 기대하는 실패하는 테스트 케이스 작성

--- test/test.js
+++ test/test.js
@@ -6,5 +6,7 @@ chai.should()
 describe ('Find Prime Factor', () => {
   it ('should return the prime factor array', () => {
     findPrime(2).should.deep.equal([ 2 ])
+    findPrime(3).should.deep.equal([ 3 ])
+    findPrime(4).should.deep.equal([ 2, 2 ])
   })
 })
