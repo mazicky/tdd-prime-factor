bb627a3623ebec47d86139ba079bf03341ffb8e3
Maz <mazicky@gmail.com>
2016-05-22 22:05:02 +0900

    기존 더미 테스트 케이스 삭제

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 import chai from 'chai'
 
-chai.should();
+chai.should()
 
-describe ('Test', () => {
-  it ('should return the index of given number', () => {
-    [1, 2, 3].indexOf(1).should.equal(0);
-  });
-});
+describe ('Find Prime Factor', () => {
+  it ('should return the prime factor array', () => {
+  })
+})
