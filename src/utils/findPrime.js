1ed5c9884fa19af49572c9b0761da9e3b578e31f
Maz <mazicky@gmail.com>
2016-05-22 22:17:38 +0900

    실패하는 테스트 케이스 작성

---
+++ src/utils/findPrime.js
@@ -0,0 +1,3 @@
+export default function findPrime(value) {
+  return value
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 import chai from 'chai'
+import findPrime from '../src/utils/findPrime'
 
 chai.should()
 
 describe ('Find Prime Factor', () => {
   it ('should return the prime factor array', () => {
+    findPrime(2).should.deep.equal([ 2 ])
   })
 })
